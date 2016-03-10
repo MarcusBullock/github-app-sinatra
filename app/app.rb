49cb97ab4bd027cdf8456d913512247f4a9b9404
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-10 10:03:29 +0000

    first morning commit

--- app/app.rb
+++ app/app.rb
@@ -16,10 +16,10 @@ class GithubApp < Sinatra::Base
   post '/create' do
     api_result = RestClient.get 'https://api.github.com/users/ScottGledhill?access_token=2a1f0daf2b47c0ef1c4886653f73d9b678b76ed5'
     jhash = JSON.parse(api_result)
-    login = jhash["login"]
-    followers = jhash["followers"]
-    avatar_url = jhash["avatar_url"]
+    # login = jhash["login"]
+    # followers = jhash["followers"]
+    # avatar_url = jhash["avatar_url"]
   end
-  # start the server if ruby file executed directly
+
   run! if app_file == $0
 end
